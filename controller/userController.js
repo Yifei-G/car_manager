7143f78fd1bb9003428f1654c4976b5dacdc210d
Yifei <yifei.ge91@gmail.com>
2020-12-24 01:50:19 +0100

    end points for user creation
    Added the end point for user creating
    
    - User can't be created using the same email address
    
    - New user's car field will be only a list of IDs
    
    - fixed attribute naming error

--- controller/userController.js
+++ controller/userController.js
@@ -6,23 +6,32 @@ function buildUser (dbData){
     let user = {};
     let car = {};
     let carList = [];
-    user.firsName = dbData.firstName;
-    user.lastNmae = dbData.lastName;
+    user.firstName = dbData.firstName;
+    user.lastName = dbData.lastName;
     user.email = dbData.email;
     user.gender = dbData.gender;
     user.URL = dbData.URL;
     if(dbData.car){
         dbData.car.forEach(model => {
-            car.carBrand = model.carBrand;
-            car.carModel = model.carModel;
-            car.URL = model.URL;
-            car.fullName = model.fullName;
-
+            // when creating new user, car will be a list of IDs
+            if(model._bsontype == "ObjectID"){
+                car = model;
+            }
+            else{
+            // when getting a detail of a user,
+            // car info will be populated
+            // so car will be a list of objects
+                car.carBrand = model.carBrand;
+                car.carModel = model.carModel;
+                car.URL = model.URL;
+                car.fullName = model.fullName;
+                
+            }
             carList.push(car);
+            car = {};      
         });
     }
     user.car = carList;
-
     return user;
 }
 
@@ -31,8 +40,63 @@ exports.userDetail = function(req, res, next){
     .populate('car','carBrand carModel')
     .exec(function(err, result){
         if(err) {return next(err)};
-        res.json({
-            user: buildUser(result),
-        })
+        if(result){
+            res.json({
+                user: buildUser(result),
+            })
+        }
+        else{
+            res.json({
+                message: 'User does not exist!!',
+            })
+        }
+
     })
 }
+
+exports.userCreate = [
+    //validation and sanitation
+    body('email').custom(value => {
+        return User.findOne({'email':value}).then(user =>{
+            if(user){
+                return Promise.reject('User already exists!!');
+            }
+        });
+    }),
+    body('firstName', 'first name can not be empty').trim().isLength({min:1}).escape(),
+    body('lastName', 'last name can not be empty').trim().isLength({min:1}).escape(),
+    body('email','the email address is invalid!').isEmail().escape(),
+    body('gender', 'gender is invalid').optional({checkFalsy:true}).trim().isLength({min:1}).escape(),
+    body('car.*').escape(),
+
+    (req, res, next) =>{
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.json({
+                errors
+            });
+            return;
+        }
+        else{
+            // Data from form is valid.
+            // Create an User object with escaped and trimmed data.
+            var user = new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                gender: req.body.gender,
+                car: req.body.car
+            });
+            user.save(function (err, result){
+                if(err) {return next(err)}
+                // Successful - return to the new user object
+                res.json({
+                    newUser: buildUser(user),
+                });
+            });
+        }
+    }
+]
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,6 @@ var userController = require('../controller/userController')
 /* GET users listing. */
 router.get('/:id/detail', userController.userDetail);
 
+router.post('/create', userController.userCreate);
+
 module.exports = router;
